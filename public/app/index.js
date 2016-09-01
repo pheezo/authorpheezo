59cc07eb061e48be0ab80e8c1cb105a95cfc6254
Afeez Adekunle <olopadeafeez@gmail.com>
2016-09-01 18:31:49 +0100

    2nd day

---
+++ public/app/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var environment_1 = require('./environment');
+exports.environment = environment_1.environment;
+var author_wizard_component_1 = require('./author-wizard.component');
+exports.AuthorWizardAppComponent = author_wizard_component_1.AuthorWizardAppComponent;
+//# sourceMappingURL=index.js.map
